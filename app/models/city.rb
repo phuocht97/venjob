c4f83312693026ab83dec04d0b4c8dcf7cec1129
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-04 13:50:55 +0700

    change handle in controller to handle in model

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,10 +2,11 @@ class City < ApplicationRecord
   has_many :city_jobs
   has_many :jobs, through: :city_jobs
 
+  scope :all_city, -> { joins(:jobs).group(:city_id).order('count(job_id) DESC') }
+  scope :vietnam, -> { where('location = 1') }
+  scope :international, -> { where('location = 0') }
+
   def self.top_city
     joins(:jobs).group(:city_id).order('count(job_id) DESC').limit(9)
-  end
-  def self.all_city
-    joins(:jobs).group(:city_id).order('count(job_id) DESC')
   end
 end
