e31196709ba889750c9e73b98faa94eb9e3a3727
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-05 13:52:45 +0700

    creating link industry, city

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,4 +5,12 @@ class JobsController < ApplicationController
     @total_job = Job.count
     @jobs_list = Job.all_job.page(params[:page]).per(20)
   end
+  def city_jobs
+    @cities = City.all
+    @industries = Industry.all
+    @city = City.find(params[:id])
+    @jobs_list = @city.jobs.all_job.page(params[:page]).per(20)
+    @total_job = Job.count
+    @result_for_job = @city.jobs.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jobs
+  get 'jobs/cities/:id', to: 'jobs#city_jobs'
+
   resources :top_pages
   resources :industries
   resources :cities
