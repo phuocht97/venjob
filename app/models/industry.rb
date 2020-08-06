745be5879e0fc3ab6e6fb5ee7ddaf58b280bb20e
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-06 09:56:51 +0700

    convert ..

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -5,4 +5,7 @@ class Industry < ApplicationRecord
   scope :top_industry, -> { joins(:jobs).group(:industry_id).order('count(job_id) DESC').limit(9) }
   scope :all_industry, -> { joins(:jobs).group(:industry_id).order('count(job_id) DESC') }
 
+  def convert_name
+    name.mb_chars.normalize(:kd).gsub(/[Đđ]/, 'd').gsub(/[^\x00-\x7F]/,'').gsub(/[\W+0-9]/,' ').downcase.to_s.split(' ').join('-')
+  end
 end
