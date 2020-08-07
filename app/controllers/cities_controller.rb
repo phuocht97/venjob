36463e25aaa3f565724024f75945e3d5539b3dfa
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-07 12:46:19 +0700

    change logic N+1 query

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,6 @@
 class CitiesController < ApplicationController
+    VIETNAM = 1
+    FOREIGN = 0
   def index
     @cities_vietnam = City.location(City::VIETNAM)
     @cities_international = City.location(City::FOREIGN)
