1f7dab8f2d2f0492dc13f3bf5e45ac509497ea87
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-07 10:11:44 +0700

    change logic city

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,6 @@
 class CitiesController < ApplicationController
   def index
-    @cities_vietnam = City.vietnam
-    @cities_international = City.international
+    @cities_vietnam = City.location(1)
+    @cities_international = City.location(0)
   end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -3,6 +3,5 @@ class City < ApplicationRecord
   has_many :jobs, through: :city_jobs
 
   scope :top_city, -> { joins(:jobs).group(:city_id).order('count(job_id) DESC').limit(9) }
-  scope :vietnam, -> { joins(:jobs).group(:city_id).order('count(job_id) DESC').where(location: 1) }
-  scope :international, -> { joins(:jobs).group(:city_id).order('count(job_id) DESC').where(location: 0) }
+  scope :location, ->(number) { joins(:jobs).group(:city_id).order('count(job_id) DESC').where(location: number) }
 end
