a8e034a4ccce6d1d5d2e09cf4510a4a364a3ce51
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-07 12:49:32 +0700

    add constant variable

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,8 @@
 class CitiesController < ApplicationController
+  VIETNAM = 1
+  FOREIGN = 0
   def index
-    @cities_vietnam = City.location(1)
-    @cities_international = City.location(0)
+    @cities_vietnam = City.location(VIETNAM)
+    @cities_international = City.location(FOREIGN)
   end
 end
