77497c1e0faef798121399275fd172d2a2b588fe
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-10 10:10:32 +0700

    add file

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,6 +5,7 @@ class City < ApplicationRecord
   VIETNAM = 1
   FOREIGN = 0
 
+
   scope :top_city, -> { joins(:jobs).group(:city_id).order('count(job_id) DESC').limit(9) }
   scope :location, ->(number) { joins(:jobs).group(:city_id).order('count(job_id) DESC').where(location: number) }
 end
