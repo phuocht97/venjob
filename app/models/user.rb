9cd7e3ff106cbfbc591d9e31115b166073ad895a
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-11 16:54:53 +0700

    Create User Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,35 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
   has_many :favorite_jobs
   has_many :jobs, through: :favorite_jobs
   has_many :job_applieds
   has_many :jobs, through: :job_applieds
   has_many :histories
   has_many :jobs, through: :histories
+
+  has_secure_password
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/login', to: 'sessions#new', via: 'get', as: :login
+  match '/logout', to: 'sessions#destroy', via: 'delete', as: :logout
+
   resources :jobs
   resources :top_pages
   resources :industries
