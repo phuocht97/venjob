d3ddaa6420c295d227c3e90eda3c514cb12d72a5
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-11 10:14:48 +0700

    change job index

--- app/helpers/convert.rb
+++ app/helpers/convert.rb
@@ -1,4 +1,4 @@
-module Convert
+module ConvertsHelper
   def to_convert(str)
     str.mb_chars.normalize(:kd).gsub(/[Đđ]/, 'd').gsub(/[^\x00-\x7F]/,'').gsub(/[\W+]/,' ').downcase.to_s.split(' ').join('-')
   end
