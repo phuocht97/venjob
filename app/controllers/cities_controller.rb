59a602e3f88816cd6be3452c4d4e6bd01559c4f7
Huỳnh Thiên Phước <phuocht@zigexn.vn>
2020-08-27 11:04:52 +0700

    fix show cities conflict

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,6 @@
 class CitiesController < ApplicationController
   def index
-    @cities_vietnam = City.location(City::VIETNAM)
-    @cities_international = City.location(City::FOREIGN)
+    @vietnam = City.location(City::VIETNAM)
+    @international = City.location(City::FOREIGN)
   end
 end
